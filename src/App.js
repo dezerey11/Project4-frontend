502369e29bf20536d85b6a573c6fe40054958ee8
Dezerey Escanuelas <dezerey11@gmail.com>
2021-06-28 19:56:02 -0700

    adds login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import AllPosts from "./pages/AllPosts";
 import AllPostsUser from "./pages/AllPostsUser";
 import SinglePost from "./pages/SinglePost";
 import SinglePostUser from "./pages/SinglePostUser";
 import Form from "./pages/Form";
 
+import "./App.css";
+
+export const GlobalCtx = React.createContext(null);
+
 function App() {
-  return <div className="App"></div>;
+  const [gState, setGState] = React.useState({
+    url: "https://project4-classifiedads-backend.herokuapp.com/",
+    // url: "http://localhost:3000/",
+    token: null,
+    ready: false,
+  });
+
+  //Check if user is logged in
+  React.useEffect(() => {
+    const token = JSON.parse(window.localStorage.getItem("token"));
+    if (token) {
+      setGState({ ...gState, token: token.token, ready: true });
+    } else {
+      setGState({ ...gState, token: null, ready: true });
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <GlobalCtx.Provider value={{ gState, setGState }}>
+        <Switch>
+          <Route exact path="/" render={(rp) => <AllPosts {...rp} />} />
+          <Route path="/login" render={(rp) => <Login {...rp} />} />
+          <Route path="/signup" render={(rp) => <Signup {...rp} />} />
+        </Switch>
+      </GlobalCtx.Provider>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/AllPosts.js
+++ src/pages/AllPosts.js
@@ -1,6 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Login from "./Login";
+import Post from "../components/post";
+import { Link, Route, Switch, Redirect, useHistory } from "react-router-dom";
+import { GlobalCtx } from "../App";
+
+const URL = "https://project4-classifiedads-backend.herokuapp.com/posts/";
 
 const AllPosts = (props) => {
-  return <h1> AllPosts Component</h1>;
+  const history = useHistory();
+  const { gState, setGState } = React.useContext(GlobalCtx);
+
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    const response = await fetch(URL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + gState.token,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  };
+
+  // if the state of token changes and the token is not ready and there is no token then redirect to login
+  useEffect(() => {
+    if (gState.ready && !gState.token) {
+      history.push("/login");
+    }
+  }, [gState.ready, gState.token]);
+
+  // if the state of token changes and there is a token then run getPosts
+  useEffect(() => {
+    if (gState.token) {
+      getPosts();
+    }
+  }, [gState.token]);
+
+  // if the state of token is not ready render nothing
+  if (!gState.ready) {
+    return null;
+  }
+
+  // return props.posts.map((post) => <Post post={post} key={post.id} />);
+  return <h1>This is the All Posts Component</h1>;
 };
 export default AllPosts;
